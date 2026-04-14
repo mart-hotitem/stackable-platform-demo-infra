3b4dd216df75649f34b5afe47c9a95b202749867
Mart van Dijk <mdijk@hotitem.nl>
2026-04-14 10:42:52 +0200

    core count is 8

--- terraform/nodepool.tf
+++ terraform/nodepool.tf
@@ -5,7 +5,7 @@ resource "ionoscloud_k8s_node_pool" "demo_pool" {
   k8s_version      = ionoscloud_k8s_cluster.demo.k8s_version
   
   node_count       = 10
-  cores_count      = 4
+  cores_count      = 8
   ram_size         = 32768   # MB = 10 GiB
   storage_size     = 30      # GB
   cpu_family       = "INTEL_SKYLAKE"
