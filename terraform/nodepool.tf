78bad6ed12e8b144bd159292a1fd5eb97aebf7b6
Mart van Dijk <mdijk@hotitem.nl>
2026-04-14 10:24:16 +0200

    increased nodepool size

--- terraform/nodepool.tf
+++ terraform/nodepool.tf
@@ -4,10 +4,10 @@ resource "ionoscloud_k8s_node_pool" "demo_pool" {
   datacenter_id    = ionoscloud_datacenter.demo.id
   k8s_version      = ionoscloud_k8s_cluster.demo.k8s_version
   
-  node_count       = 2
+  node_count       = 10
   cores_count      = 4
-  ram_size         = 10240   # MB = 10 GiB
-  storage_size     = 200      # GB
+  ram_size         = 32768   # MB = 10 GiB
+  storage_size     = 30      # GB
   cpu_family       = "INTEL_SKYLAKE"
   availability_zone     = "AUTO"
   storage_type          = "SSD"
